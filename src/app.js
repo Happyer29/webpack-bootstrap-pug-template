f86e8b1343c9c6db751045e52687fb282e9e3c7f
happyer29 <happyer.present@gmail.com>
2021-08-25 20:48:46 +0300

    Some changes with project structure -test scripts
    "start": "npm-run-all --parallel lint:watch test:watch build",
    "test": "mocha ./src/**/*.test.js --compilers js:babel-core/register",
    "test:watch": "npm run test -- --watch",

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import { hello } from './scripts/another-file';
+import './styles/style.scss';
+//import bootstrap from 'bootstrap'
+
+let val = hello();
+console.log(val);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const miniCss = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "development", //TODO console parameter for development/prod
+  devtool: "source-map", //TODO console parameter with/without maps
   entry: {
     app: './src/app.js'
   },
@@ -10,21 +12,40 @@ module.exports = {
   watch: true,
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "[contenthash].bundle.js",
+    filename: "[name].bundle.js",
   },
-  resolve: {
-    extensions: [".json", ".js", ".jsx", "*",],
-  },
+
+  // resolve: { TODO learn modules
+  //   extensions: [".json", ".js", ".jsx", "*",],
+  // },
 
   module: {
     rules: [
-      {
+      { //for using babel
+        test: /^app\/\S*\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
+      { //For scss
+        test: /\.(s*)css$/,
+        use: [miniCss.loader, "css-loader", "sass-loader"],
+      },
+      { //For pug
          test: /\.pug$/,
          use: ["pug-loader"],
       },
     ],
   },
   plugins: [
+    new miniCss({ //TODO wtf is this
+      filename: "./styles/style.css",
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.pug'
     })
